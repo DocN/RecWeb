3575df3f13ca420e76179b5dff6293f0d1b4b465
DrN <DrN@DESKTOP-GORGVHP>
2018-04-25 23:02:29 -0700

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+
 //bootstrap memes
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
+//route imports
+import {RouterModule, Routes} from '@angular/router';
+import { LoginPortalComponent } from './login-portal/login-portal.component';
+
+const appRoutes:Routes = [
+  {
+    path: 'login',
+    component: LoginPortalComponent,
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPortalComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes, {useHash: true}),
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
