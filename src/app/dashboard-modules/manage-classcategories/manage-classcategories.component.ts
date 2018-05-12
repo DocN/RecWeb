f139c9b930596375c235c92fda8b1e8566760d16
DrN <031247ryan@gmail.com>
2018-05-11 20:43:14 -0700

    added filter for edit categories

--- src/app/dashboard-modules/manage-classcategories/manage-classcategories.component.ts
+++ src/app/dashboard-modules/manage-classcategories/manage-classcategories.component.ts
@@ -16,6 +16,16 @@ export class ManageClasscategoriesComponent implements OnInit {
   private addClassCategory:any={};
   private successMessageInternal;
   private formErrors;
+  private categoryFilter: any={};
+
+  //edit category table
+  private editCategoryTable: any={};
+  private categoryTypeFilter = 1;
+  private editCategoryFilter: any={};
+
+  //select category
+  private selectedID;
+
 
   constructor(private http: HttpClient, private jsonURL:GetJsonService, private dashroute:DashrouteService) { }
 
@@ -23,6 +33,7 @@ export class ManageClasscategoriesComponent implements OnInit {
     this.route = 0;
     this.subRoute = 0;
     this.addClassCategory.hexColor = "#000000";
+    this.getClassCategorytable();
   }
 
   goCreateCategory() {
@@ -39,11 +50,11 @@ export class ManageClasscategoriesComponent implements OnInit {
     this.dashroute.currentRoute = '/';
     this.dashroute.currentTitle = "Dashboard";
   }
-  
 
   onChangeColor($event) {
     this.addClassCategory.hexColor = $event;
   }
+
   createCategoryButton() {
     if(this.validateCreateCategoryForm() == false) {
       return;
@@ -70,6 +81,39 @@ export class ManageClasscategoriesComponent implements OnInit {
     );
   }
 
+  getClassCategorytable() {
+    this.editCategoryTable.categoryID = [];
+    this.editCategoryTable.categoryName = [];
+    this.editCategoryTable.hexColor = [];
+    this.editCategoryTable.filtered = [];
+    let data = {};
+    this.http.post(this.jsonURL.getClassCategoriesURL(), data)
+      .subscribe(
+        (res) => {
+          if(!res) {
+            return;
+          }
+          if(res.toString() != "") {
+            console.log(res);
+            var count = 0;
+            while(res[count] != null) {
+              count = count +1;
+            }
+            for(let i =0; i < count; i++) {
+              this.editCategoryTable.categoryID.push(res[i].categoryID);
+              this.editCategoryTable.categoryName.push(res[i].categoryName);
+              this.editCategoryTable.hexColor.push(res[i].hexColor);
+              this.editCategoryTable.filtered.push(1);
+            }
+          }
+        },
+        err => {
+          console.log(err);
+          //finish loading
+        }
+    );
+  }
+
   validateCreateCategoryForm() {
     this.formErrors = [];
     var validate = true;
@@ -79,6 +123,71 @@ export class ManageClasscategoriesComponent implements OnInit {
       validate = false;
     }
     return validate;
+  }
+
+  setCategoryFilter($val) {
+    //selected filter values
+    this.categoryTypeFilter = $val;
+    this.applyCategoryFilter();
+  }
+
+  resetCategoryFilter() {
+    for(let i =0; i < this.editCategoryTable.filtered.length; i++) {
+      this.editCategoryTable.filtered[i] = 1;
+    }
+  }
+
+  applyCategoryFilter() {
+    this.resetCategoryFilter();
+    var filterOption = this.categoryTypeFilter;
+    if(!this.editCategoryFilter.value) {
+      return;
+    }
+    var currentFilter = this.editCategoryFilter.value.toString().toLowerCase();
+    for(let i =0; i < this.editCategoryTable.filtered.length; i++) {
+      //uid filter
+      if(filterOption == 1) {
+        if(!this.editCategoryTable.categoryID[i]) {
+          return;
+        }
+        var currentVal = this.editCategoryTable.categoryID[i].toString().toLowerCase();
+        if(currentVal.indexOf(currentFilter) == -1) {
+          this.editCategoryTable.filtered[i] = 0;
+        }
+      } 
+      //First filter
+      else if(filterOption == 2) {
+        if(!this.editCategoryTable.categoryName[i]) {
+          return;
+        }
+        var currentVal = this.editCategoryTable.categoryName[i].toString().toLowerCase();
+        if(currentVal.indexOf(currentFilter) == -1) {
+          this.editCategoryTable.filtered[i] = 0;
+        }
+      } 
+      //Last filter
+      else if(filterOption == 3) {
+        if(!this.editCategoryTable.hexColor[i]) {
+          return;
+        }
+        var currentVal = this.editCategoryTable.hexColor[i].toString().toLowerCase();
+        if(currentVal.indexOf(currentFilter) == -1) {
+          this.editCategoryTable.filtered[i] = 0;
+        }
+      }
+    }
+  }
+
+  selectCategory($event) {
+    var currentID = $event["srcElement"]["id"];
+    currentID = currentID.slice(8);
+    this.selectedID = currentID;
+    this.route = '2';
+    this.subRoute = '1';
+    
+    this.editCategoryTable.categoryID = this.editCategoryTable.categoryID[currentID];
+    this.editCategoryTable.categoryName = this.editCategoryTable.categoryName[currentID];
+    this.editCategoryTable.hexColor = this.editCategoryTable.hexColor[currentID];
   }
 
 }
