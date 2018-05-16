8e83a349a714cf3d29c85b8ad0e9261034104843
DrN <031247ryan@gmail.com>
2018-05-16 16:13:08 -0700

    added date filter to reviews

--- src/app/dashboard-modules/view-reviews/view-reviews.component.ts
+++ src/app/dashboard-modules/view-reviews/view-reviews.component.ts
@@ -15,13 +15,20 @@ export class ViewReviewsComponent implements OnInit {
   private route;
   private subRoute;
   private reviewDataTable: any= {};
-
+  private filterBeginDate;
+  private filterEndDate;
+  private filterBeginUnix;
+  private filterEndUnix;
   constructor(private http: HttpClient, private jsonURL:GetJsonService, private dashroute:DashrouteService) { }
 
   ngOnInit() {
     this.route = 1;
     this.subRoute = 0;
     this.loadReviewData();
+    this.filterBeginDate = new Date();
+    this.filterEndDate = new Date();
+    this.filterBeginUnix = 0;
+    this.filterEndUnix = 10000000000000000000;
   }
 
   loadReviewData() {
@@ -76,5 +83,17 @@ export class ViewReviewsComponent implements OnInit {
     var size = val/5*100;
     console.log(size);
     return size + '%';
+  }
+
+  updateTime() {
+    console.log("update time");
+    this.filterBeginDate.setHours(0,0,0,0);
+    this.filterEndDate.setHours(0,0,0,0);
+    this.filterBeginUnix = Math.round(this.filterBeginDate.getTime()/1000);
+    this.filterEndUnix = Math.round(this.filterEndDate.getTime()/1000);
+    if(this.filterBeginUnix == this.filterEndUnix) {
+      this.filterEndDate.setHours(23,59,0,0);
+      this.filterEndUnix = Math.round(this.filterEndDate.getTime()/1000);
+    }
   }
 }
